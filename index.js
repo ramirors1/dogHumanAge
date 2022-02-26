3f5819aec03b8a2ebab4f639ac79b5d466674195
Your Github User Name <ramirezr@protonmail.com>
2022-02-26 08:12:06 -0600

    finished calculation

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-prompt("What is your dogs Age?")
-
-// var humanAge = (dogAge -2) x 4 + 21
+var dogAge = prompt("What is your dogs Age?")
+var humanAge = (dogAge -2) * 4 + 21
+alert("Your dog's age in human years is " + humanAge + " human years.");
